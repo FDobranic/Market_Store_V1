70571b20bf3a73ac96aa0409450ba2961d036f32
filipdobranic1234 <filipdobranic@hotmail.com>
2020-06-29 20:50:59 +0200

    first commit

---
+++ MarketStore/Model/DiscountCard.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketStore.Model
+{
+    public enum CardType { Bronze, Silver, Gold}
+    class DiscountCard
+    {
+        public string OwnerInfo { get; set; }
+        public double Turnover { get; set; }
+        private double DiscountRate { get; set; }
+
+        public CardType Type { get; set; }
+
+        public DiscountCard(double Turnover, CardType Type)
+        {
+            this.Turnover = Turnover;
+            this.Type = Type;
+        }
+
+        public double calculateDiscount(double purchaseValue)
+        {
+            return purchaseValue * DiscountRate / 100;
+        }
+
+        public double calculateTotal(double purchaseValue)
+        {
+            return purchaseValue - calculateDiscount(purchaseValue);
+        }
+
+        public double calculateRate()
+        {
+            if(Type == CardType.Bronze)
+            {
+                if(Turnover < 100)
+                {
+                    DiscountRate = 0;
+                }
+                else if(Turnover >= 100 && Turnover <= 300)
+                {
+                    DiscountRate = 1;
+                }
+                else
+                {
+                    DiscountRate = 2.5;
+                }
+            }
+            else if(Type == CardType.Silver)
+            {
+                if(Turnover > 300)
+                {
+                    DiscountRate = 3.5;
+                }
+                else
+                {
+                    DiscountRate = 2;
+                }
+            }
+            else
+            {
+                if(Turnover >= 800)
+                {
+                    DiscountRate = 10;
+                }
+                else if(Turnover >= 100)
+                {
+                    DiscountRate = (Turnover + 200) / 100;
+                }
+                else
+                {
+                    DiscountRate = 2;
+                }
+            }
+            return DiscountRate;
+        }
+    }
+}
---
+++ MarketStore/Program.cs
@@ -0,0 +1,41 @@
+﻿using MarketStore.Model;
+using System;
+
+namespace MarketStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscountCard bronzeCard = new DiscountCard(0, CardType.Bronze);
+
+            double purchaseValue = 1300;
+
+            Console.WriteLine("Bronze card: ");
+            Console.WriteLine("Purchase value: ${0}", purchaseValue);
+            Console.WriteLine("Discount rate: {0}%", bronzeCard.calculateRate());
+            Console.WriteLine("Discount: ${0}", bronzeCard.calculateDiscount(purchaseValue));
+            Console.WriteLine("Total: ${0}", bronzeCard.calculateTotal(purchaseValue));
+
+            Console.WriteLine();
+
+            DiscountCard silverCard = new DiscountCard(600, CardType.Silver);
+
+            Console.WriteLine("Silver card: ");
+            Console.WriteLine("Purchase value: ${0}", purchaseValue);
+            Console.WriteLine("Discount rate: {0}%", silverCard.calculateRate());
+            Console.WriteLine("Discount: ${0}", silverCard.calculateDiscount(purchaseValue));
+            Console.WriteLine("Total: ${0}", silverCard.calculateTotal(purchaseValue));
+
+            Console.WriteLine();
+
+            DiscountCard goldCard = new DiscountCard(500, CardType.Gold);
+
+            Console.WriteLine("Gold card: ");
+            Console.WriteLine("Purchase value: ${0}", purchaseValue);
+            Console.WriteLine("Discount rate: {0}%", goldCard.calculateRate());
+            Console.WriteLine("Discount: ${0}", goldCard.calculateDiscount(purchaseValue));
+            Console.WriteLine("Total: ${0}", goldCard.calculateTotal(purchaseValue));
+        }
+    }
+}
